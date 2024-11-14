acd3f81dd53f84b4856dbb36784ceb8937a57338
umar7shaikh <umarskpune@gmail.com>
2024-11-15 00:54:48 +0530

    addedroutesandnavbarcomponet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Route , Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import Navbar from './Components/Navbar'
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
-
+      <Navbar/>
       <Routes>
         <Route path='/' element={<Home />} /> 
+        <Route path='/doctors' element={<Doctors />} /> 
+        <Route path='/doctors/:speciality' element={<Doctors />} /> 
+        <Route path='/login' element={<Login/>} /> 
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointment' element={<MyAppointments />} />
+        <Route path='/my-appointment/:docid' element={<MyAppointments />} />
       </Routes>
       
     </div>
