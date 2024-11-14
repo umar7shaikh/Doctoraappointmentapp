212691491cf49f43cc6a9853f4109f23a2f0c125
umar7shaikh <umarskpune@gmail.com>
2024-11-15 00:16:20 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Route , Routes } from 'react-router-dom'
+import Home from './pages/Home'
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+
+      <Routes>
+        <Route path='/' element={<Home />} /> 
+      </Routes>
+      
+    </div>
+  )
+}
+
+export default App
+
+
